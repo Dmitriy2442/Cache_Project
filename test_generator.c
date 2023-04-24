eac864e0b0577d6b673b1030fcedc102645b8a1e
Виталий Клименко <klimenko.ve@phystech.edu>
2023-04-24 22:41:53 +0300

    pseudo-random number generator

---
+++ test_generator.c
@@ -0,0 +1,30 @@
+//-----------------------------------------------------------------------------
+//
+// Early version of the test generator for LIRS
+//
+//-----------------------------------------------------------------------------
+//
+// Explanation: the variable m is responsible for the number of 
+pseudo-random 
+// numbers needed, and the variable n is responsible for the number of 
+pages 
+// used in the LIRS algorithm. Function TESTING_THREAD will be used in our 
+// further work.
+// 
+//-----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int *testing_thread(int m, int n)
+{
+    int *a;
+    a = (int *) calloc(m, sizeof(int));
+    srand(time(NULL));
+    for (int i = 0; i < m; ++i)
+    {
+        a[i] = rand() % n;
+    }
+    return a;
+}
---
+++ test_generator.h
@@ -0,0 +1,9 @@
+#ifndef TEST_GENERATOR_H
+#define TEST_GENERATOR_H
+
+//Realization of function, that creates m pseudo-random numbers from 0 to 
+n-1
+int *testing_thread(int m, int n);  //returns an int-pointer to array of 
+numbers with length m 
+
+#endif /*TEST_GENERATOR_H*/
