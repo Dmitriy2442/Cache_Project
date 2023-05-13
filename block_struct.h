1702681f5fc34e1209eef9d676ab2a0297b62e3a
Dmitriy <volozhinov.ds@phystech.edu>
2023-05-13 04:24:32 +0300

    Added: singly-connected lists functions returned for hash usage. Changed: block struct now contains only number and flags

--- block_struct.h
+++ block_struct.h
@@ -3,11 +3,10 @@
 
 struct block
 {
-    int number;          //page number without hash-function used
-    int IRR;             //IRR of the block
-    int HIR;             //LIR/HIR block status flag, 0 - LIR, 1 - HIR
-    int residency;       //cache residency flag, 0 - non-resident block, 1 - resident block
-    int last_access;     //the "time" that was on the counter last time this block got accessed
+    int number;             //page number without hash-function used
+    int HIR;                //LIR/HIR block status flag, 0 - LIR, 1 - HIR
+    int cache_residency;    //cache residency flag, 0 - non-resident block, 1 - resident block
+    int stack_residency;    //stack_residency
 };
 
 #endif /*BLOCK_STRUCT_H*/
