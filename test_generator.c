bd3e1c82491f05920b7ef88a4f6ba33e55259962
Виталий Клименко <klimenko.ve@phystech.edu>
2023-05-15 15:13:41 +0300

    Edited generator of pseudo-random numbers

--- test_generator.c
+++ test_generator.c
@@ -1,10 +1,9 @@
 //-----------------------------------------------------------------------------
 //
 // Variable m is responsible for the number of
-// pseudo-random numbers needed, and the variable n is responsible for the
-// number of pages used in the LIRS algorithm. Function TESTING_THREAD 
-will
-// be used in our further work.
+// pseudo-random numbers needed, and the variable n is responsible 
+// for the number of pages used in the LIRS algorithm. Function 
+// TESTING_THREAD will be used in our further work.
 //
 //-----------------------------------------------------------------------------
 
@@ -26,4 +25,11 @@ FILE *testing_thread(int m, long long n)
     }
     free(a);
     return file;
+}
+
+int main() {
+  int m, n;
+  scanf("%d %d", &m, &n);
+  FILE *file = testing_thread(m, n);
+  fclose(file);
 }
